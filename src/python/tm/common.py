b20043acb0c7075eabd22256f7b6bc1002324dce
Jesse Farnham <jfarnham20@gmail.com>
2015-01-20 23:20:41 -0500

    first commit

---
+++ src/python/tm/common.py
@@ -0,0 +1,12 @@
+import enum
+
+
+class Terrain(enum):
+    PLAINS = 0
+    SWAMP = 1
+    LAKE = 2
+    FOREST = 3
+    MOUNTAINS = 4
+    WASTELAND = 5
+    DESERT = 6
+    RIVER = 7
---
+++ src/python/tm/pieces.py
@@ -0,0 +1,89 @@
+# priests, structures, and bridges are limited in quantity. Workers and coins aren't.
+
+
+class GamePiece(object):
+    pass
+
+
+class Structure(GamePiece):
+    pass
+
+
+class Dwelling(Structure):
+    pass
+
+
+class TradingHouse(Structure):
+    pass
+
+
+class Temple(Structure):
+    pass
+
+
+class Sanctuary(Structure):
+    pass
+
+
+class Stronghold(Structure):
+    pass
+
+
+class Town(GamePiece):
+    pass
+
+
+class ScoringTile(GamePiece):
+    pass
+
+
+class BonusCard(GamePiece):
+    pass
+
+
+class FavorCard(GamePiece):
+    pass
+
+
+class HundredPointToken(GamePiece):
+    pass
+
+
+class GameEndToken(GamePiece):
+    pass
+
+
+class ActionToken(GamePiece):
+    pass
+
+
+class PlayerOrderToken(GamePiece):
+    pass
+
+
+class PowerToken(GamePiece):
+    pass
+
+
+class Coin(GamePiece):
+    pass
+
+
+class Worker(GamePiece):
+    pass
+
+
+class TerrainTile:
+    pass
+
+
+class Priest(GamePiece):
+    pass
+
+
+class Marker(GamePiece):
+    pass
+
+
+class Bridge(GamePiece):
+    pass
