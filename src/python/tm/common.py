ec3dd8905c1e1a8975f11042e3bab639d9a56d13
Jesse Farnham <jfarnham20@gmail.com>
2015-01-28 00:04:38 -0500

    add cult board

--- src/python/tm/common.py
+++ src/python/tm/common.py
@@ -2,6 +2,7 @@ import enum
 
 CONF_DIR = '../../conf'
 
+
 class Terrain(enum.Enum):
     PLAINS = 0
     SWAMP = 1
@@ -11,3 +12,29 @@ class Terrain(enum.Enum):
     WASTELAND = 5
     DESERT = 6
     RIVER = 7
+
+    def to_texture(self):
+        return self.name
+
+
+class Element(enum.Enum):
+    FIRE = 0
+    WATER = 1
+    EARTH = 2
+    AIR = 3
+
+    def to_texture(self):
+        return self.name
+
+
+class Direction(enum.Enum):
+    NE = 30
+    E = 90
+    SE = 150
+    SW = 210
+    W = 270
+    NW = 330
+
+
+POWER_TEXTURE = "POWER"
+TOWN_KEY_TEXTURE = "POWER"
